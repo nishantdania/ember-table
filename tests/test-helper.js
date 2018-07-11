2968954db789f05254133a86dd92025ec6169ebb
nishantdania <nishantdania@gmail.com>
2018-07-11 17:20:09 +0800

    Remove all js

--- tests/test-helper.js
+++
@@ -1,9 +0,0 @@
-import resolver from './helpers/resolver';
-import { setResolver } from 'ember-qunit';
-import { start } from 'ember-cli-qunit';
-import 'dummy/app';
-import registerRAFWaiter from 'ember-raf-scheduler/test-support/register-waiter';
-
-registerRAFWaiter();
-setResolver(resolver);
-start();
